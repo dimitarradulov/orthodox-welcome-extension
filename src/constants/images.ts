32a4c2ea4c6b21f729ed228b4ace31efe9dd4173
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-11-09 13:08:24 +0200

    increase num of images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,3 @@
-export const NUMBER_OF_IMAGES: Readonly<number> = 20;
+export const NUMBER_OF_IMAGES: Readonly<number> = 25;
 export const S3_BASE_IMAGES_URL: Readonly<string> =
   "https://orthodoxwelcome.s3.amazonaws.com/backgrounds";
